992a384ada9a80214ed80ab4e624bd8bb066e26a
xezon88 <xezon@bk.ru>
2021-08-25 11:52:25 +0500

    Работа с разными экранами js

--- js/main.js
+++ js/main.js
@@ -5,8 +5,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const quiz = document.querySelector(".quiz");
     const passTestButton = document.querySelector(".pass-test__button");
     const form = document.querySelector(".quiz-body__form");
+    const inputs = document.querySelectorAll("input");
     const formItems = form.querySelectorAll("fieldset");
+    const btnsNext = form.querySelectorAll(".form-button_btn-next");
+    const btnsPrev = form.querySelectorAll(".form-button_btn-prev");
 
+    btnsNext.forEach((btn, btnIndex) => {
+        btn.addEventListener('click', (event) => {
+            event.preventDefault();
+
+
+            formItems[btnIndex].style.display = "none";
+            formItems[btnIndex + 1].style.display = "block";
+        });
+    });
+
+    btnsPrev.forEach((btn, btnIndex) => {
+        btn.addEventListener('click', (event) => {
+            event.preventDefault();
+
+
+            formItems[btnIndex + 1].style.display = "none";
+            formItems[btnIndex].style.display = "block";
+        });
+    });
 
     formItems.forEach((formItem, formItemIndex) => {
         if (formItemIndex === 0) {
@@ -15,7 +37,33 @@ document.addEventListener("DOMContentLoaded", function() {
             formItem.style.display = "none";
         }
 
+        formItem.addEventListener('change', (event) => {
+            const target = event.target;
+
+
+            if (target.classList.contains("form__input")) {
+                const inputs = formItem.querySelectorAll(".form__input");
+
+                inputs.forEach((input) => {
+                    if (input === target) {
+                        input.parentNode.classList.toggle("active-checkbox");
+                    } else {
+                        input.parentNode.classList.remove("active-checkbox");
+                        input.checked = false;
+                    }
+                });
+            } else {
+                return;
+            }
+        });
     });
+
+    inputs.forEach((input) => {
+        const parent = input.parentNode;
+        input.checked = false;
+        parent.classList.remove("active-radio");
+        parent.classList.remove("active-checkbox");
+    })
 
     overlay.style.display = "none";
     quiz.style.display = "none";
